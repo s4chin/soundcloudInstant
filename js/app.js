3bb529574e2bb7276373b60ce69f56deade63e43
Sachin Kelkar <sachinkel19@gmail.com>
2015-06-27 21:51:26 +0530

    Make songs loop

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var page_size = 15; //Maximum songs in list
     var track_list;
     var search_list = ["Take Me to Church", "Burn", "Dream On"];
+    var index = 0;
 
     SC.initialize({
         client_id: 'a9d59742643cc1ff818f6e2f79b3b6c1'
@@ -41,7 +42,8 @@ $(document).ready(function() {
             }
             else {
                 track_list = tracks;
-                playTrack(track_list.shift());
+                index = 0;
+                playTrack(track_list[index]);
             }
         });
     }
@@ -60,7 +62,14 @@ $(document).ready(function() {
 
     function next() {
         if (track_list.length > 0) {
-            playTrack(track_list.shift())
+            if (index < track_list.length - 1) {
+                index += 1;
+                playTrack(track_list[index]);
+            }
+            else {
+                index = 0;
+                playTrack(track_list[index]);
+            }
         }
         else {
 
@@ -73,6 +82,10 @@ $(document).ready(function() {
         var query = $('#sterm').val();
 
         if(query == '') {
+            return;
+        }
+
+        if (e.keyCode == 9 || e.keyCode == 16 || e.keyCode == 17 || e.keyCode == 18 || e.keyCode == 91 || e.keyCode == 92) {
             return;
         }
 
