c9036b6e732cc2743353ad355652b8f19dfff540
Sachin Kelkar <sachinkel19@gmail.com>
2015-06-27 14:21:47 +0530

    Add play fn

--- js/app.js
+++ js/app.js
@@ -10,19 +10,31 @@ $(document).ready(function() {
 
     var scwidget = document.getElementById('scwidget');
     scwidget.src = "https://w.soundcloud.com/player/?url=http://api.soundcloud.com/users/1539950/favorites";
+    var widget = SC.Widget(scwidget);
 
     var qsearch = function(query) {
-        SC.get('/tracks', { q: query }, function(tracks) {
+        SC.get('/tracks', { q: query, limit: page_size }, function(tracks) {
             if(tracks.length == 0) {
 
             }
             else {
-
+                playTrack(track[0]);
             }
         });
     }
     
-    
+    var playTrack = function(track) {
+        widget.load(track.uri, {
+            auto_play: true,
+            buying: false,
+            liking: false,
+            download: true,
+            sharing: false,
+            show_artwork: true,
+            show_comments: false,
+        });
+    }
+
     $('#sterm').keyup(function(e) {
         e.preventDefault();
 
