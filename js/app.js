15f2c956bb587d042759ff57f502801f5b1586ee
Sachin Kelkar <sachinkel19@gmail.com>
2015-06-27 14:54:29 +0530

    Add init

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
     var page_size = 15; //Maximum songs in list
     var track_list;
+    var search_list = ["Take Me to Church", "Burn", "Dream On"];
 
     SC.initialize({
         client_id: 'a9d59742643cc1ff818f6e2f79b3b6c1'
@@ -10,14 +11,22 @@ $(document).ready(function() {
     console.log("ready");
 
     var scwidget = document.getElementById('scwidget');
-    scwidget.src = "https://w.soundcloud.com/player/?url=http://api.soundcloud.com/users/1539950/favorites";
     var widget = SC.Widget(scwidget);
+    winit();
+
+
 
     widget.bind(SC.Widget.Events.READY, function() {
         widget.bind(SC.Widget.Events.FINISH, function(e) {
             next();
         });
     });
+
+    var winit = function() {
+        var qinit = search_list[Math.floor(Math.random()*search_list.length)];
+        document.getElementById('sterm').value = qinit;
+        qsearch(qinit);
+    }
 
     var qsearch = function(query) {
         SC.get('/tracks', { q: query, limit: page_size }, function(tracks) {
@@ -48,7 +57,7 @@ $(document).ready(function() {
             playTrack(track_list.shift())
         }
         else {
-            
+
         }
     }
 
