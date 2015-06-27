0dcbeb37c9bdb784e1e7168ff327bfe20569100b
Sachin Kelkar <sachinkel19@gmail.com>
2015-06-27 13:17:42 +0530

    Add search

--- js/app.js
+++ js/app.js
@@ -11,8 +11,29 @@ $(document).ready(function() {
     var scwidget = document.getElementById('scwidget');
     scwidget.src = "https://w.soundcloud.com/player/?url=http://api.soundcloud.com/users/1539950/favorites";
 
-    SC.get('/tracks', { genres: 'punk', bpm: { from: 120 } }, function(tracks) {
-    console.log(tracks);
-    });
+    var qsearch = function(query) {
+        SC.get('/tracks', { q: query }, function(tracks) {
+            if(tracks.length == 0) {
+
+            }
+            else {
+
+            }
+        });
+    }
     
+    
+    $('#sterm').keyup(function(e) {
+        e.preventDefault();
+
+        var query = $('#sterm').val();
+
+        if(query == '') {
+            return;
+        }
+
+        qsearch(query);
+
+    });
+
 });
